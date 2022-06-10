c4614f9056f6829b8978803d80d6196c73a4b948
Javier <javiaragoneses1@gmail.com>
2022-06-10 18:42:16 +0200

    Creación de menu para copiar archivos a directorio

---
+++ src/Server/GUI/TreeInterpreter/TreeGUI/Menus/CopyMenu.java
@@ -0,0 +1,34 @@
+package Server.GUI.TreeInterpreter.TreeGUI.Menus;
+
+
+import Server.Connections.Streams;
+
+import Server.GUI.TreeInterpreter.TreeUtils.Action;
+import Server.GUI.TreeInterpreter.DirectoryTreeGUI.DirectoryDiskMenu;
+import Server.GUI.TreeInterpreter.TreeUtils.TreeMenu;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+
+
+public class CopyMenu extends TreeMenu {
+
+    private final Streams stream;
+    private final JDialog dialog;
+    private final JMenu copyItem;
+
+    public CopyMenu(JTree tree, Streams stream, JDialog dialog, JMenu copyItem) {
+        super(tree);
+        this.stream = stream;
+        this.dialog = dialog;
+        this.copyItem = copyItem;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+        copyItem.removeAll();
+        stream.executor.submit(new DirectoryDiskMenu(copyItem, stream, dialog, getSelectedPaths(), Action.COPY));
+    }
+
+
+}
