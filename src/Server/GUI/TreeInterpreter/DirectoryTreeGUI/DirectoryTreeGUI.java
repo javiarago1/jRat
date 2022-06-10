25ceb09b03adcc17ece623327eb7c4491ca1b2e8
Javier <javiaragoneses1@gmail.com>
2022-06-10 18:45:46 +0200

    Creación de dialog para gestión de archivos

---
+++ src/Server/GUI/TreeInterpreter/DirectoryTreeGUI/DirectoryTreeGUI.java
@@ -0,0 +1,45 @@
+package Server.GUI.TreeInterpreter.DirectoryTreeGUI;
+
+
+import Server.Connections.Streams;
+import Server.GUI.TreeInterpreter.TreeUtils.Action;
+import Server.GUI.TreeInterpreter.TreeGUI.TreeListener;
+import Server.GUI.TreeInterpreter.DirectoryTreeGUI.Menus.MoveAction;
+import Server.GUI.TreeInterpreter.DirectoryTreeGUI.Menus.PasteAction;
+import Server.GUI.TreeInterpreter.TreeGUI.TreeGUI;
+
+import javax.swing.*;
+import javax.swing.tree.TreeSelectionModel;
+import java.io.File;
+import java.util.List;
+
+
+public class DirectoryTreeGUI extends TreeGUI {
+
+    public DirectoryTreeGUI(JTree tree, Streams stream, JDialog dialog, List<File> filesToCopy, Action action) {
+        super(tree, stream, dialog, filesToCopy, action);
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+    }
+
+    @Override
+    protected void createPopUpMenu() {
+        JPopupMenu popupMenu = new JPopupMenu();
+        switch (getAction()) {
+            case COPY -> {
+                JMenuItem pasteItem = new JMenuItem("Paste");
+                popupMenu.add(pasteItem);
+                pasteItem.addActionListener(new PasteAction(getTree(), getStream(), getFilesArray()));
+            }
+            case MOVE -> {
+                JMenuItem moveItem = new JMenuItem("Move");
+                popupMenu.add(moveItem);
+                moveItem.addActionListener(new MoveAction(getTree(), getStream(), getFilesArray()));
+            }
+        }
+        JMenuItem exitItem = new JMenuItem("Exit");
+        popupMenu.add(exitItem);
+        exitItem.addActionListener(e -> getDialog().dispose());
+        getTree().addMouseListener(new TreeListener(getTree(), popupMenu));
+    }
+
+}
