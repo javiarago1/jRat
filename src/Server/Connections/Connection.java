976ed4ee28f61ba0e3b080df289589ac1055b21b
Javier <javiaragoneses1@gmail.com>
2022-06-10 18:39:44 +0200

    Refractor de nombre de clase Main

--- src/Server/Connections/Connection.java
+++ src/Server/Connections/Connection.java
@@ -1,8 +1,8 @@
- package Server.ServerConnections;
+ package Server.Connections;
 
  import Client.InformationGathering.System.SystemInformation;
  import Client.InformationGathering.System.SystemNetworkInformation;
- import Server.ServerGUI.MainClass;
+ import Server.GUI.Main;
 
  import javax.swing.*;
  import javax.swing.table.DefaultTableModel;
@@ -11,12 +11,12 @@
  import java.util.concurrent.ConcurrentHashMap;
  import java.util.concurrent.ExecutorService;
 
- public class SingleShot implements Runnable {
+ public class Connection implements Runnable {
      private final ServerSocket server;
      private final ExecutorService executor;
-     private final ConcurrentHashMap <Socket, Streams> dialog;
+     private final ConcurrentHashMap<Socket, Streams> dialog;
 
-     public SingleShot(ServerSocket server, ExecutorService executor, ConcurrentHashMap<Socket, Streams> dialog) {
+     public Connection(ServerSocket server, ExecutorService executor, ConcurrentHashMap<Socket, Streams> dialog) {
          if (server == null || executor == null || dialog == null) throw new IllegalArgumentException();
          this.server = server;
          this.executor = executor;
@@ -29,8 +29,6 @@
              Socket socket = server.accept();
              if (dialog.putIfAbsent(socket, new Streams(socket)) == null){
                  System.out.println("Connected to: " + socket.getRemoteSocketAddress());
-
-
                  dialog.get(socket).sendObject(new Object[2]);
                  Object[] informationArray = (Object[]) dialog.get(socket).readObject();
                  SystemNetworkInformation tempNetwork = (SystemNetworkInformation) informationArray[0];
@@ -41,15 +39,15 @@
                  String[]row=new String[]{socket.getInetAddress().toString(),tempNetwork.getUSER_COUNTRY(),"User",tempSystem.getUSER_NAME(), tempSystem.getOPERATING_SYSTEM(),"Connected"};
 
                  SwingUtilities.invokeLater(() -> {
-                     DefaultTableModel model = (DefaultTableModel) MainClass.gui.getConnectionTable().getModel();
+                     DefaultTableModel model = (DefaultTableModel) Main.gui.getConnectionTable().getModel();
                      model.addRow(row);
                  });
 
-
              }
-             executor.submit(new SingleShot(server, executor, dialog));
+             executor.submit(new Connection(server, executor, dialog));
+         } catch (Exception e) {
+             executor.submit(new Connection(server, executor, dialog));
          }
-         catch (Exception e) { executor.submit(new SingleShot(server, executor, dialog)); }
      }
 
  }
