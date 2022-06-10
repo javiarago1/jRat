62b33fc57a3aad34248eae331793bc787038c665
Javier <javiaragoneses1@gmail.com>
2022-06-10 18:37:14 +0200

    Refractor y añadido a ThreadPool (Executor)

--- src/Server/GUI/TableUtils/FileBrowser/BrowserListener.java
+++ src/Server/GUI/TableUtils/FileBrowser/BrowserListener.java
@@ -1,7 +1,7 @@
-package Server.ServerGUI.TableUtils;
+package Server.GUI.TableUtils.FileBrowser;
 
-import Server.ServerConnections.Streams;
-import Server.ServerGUI.GetSYS;
+import Server.Connections.Streams;
+import Server.GUI.TableUtils.Configuration.GetSYS;
 
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
@@ -9,17 +9,16 @@ import javax.swing.event.MenuListener;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class BrowserMenuListener implements MenuListener {
+public class BrowserListener implements MenuListener {
 
     private final JMenu browserMenu;
     private final JTable table;
-    private final ConcurrentHashMap<Socket,Streams> map;
+    private final ConcurrentHashMap<Socket, Streams> map;
 
-    public BrowserMenuListener(JTable table, JMenu browserMenu , ConcurrentHashMap<Socket,Streams> map){
-        this.browserMenu=browserMenu;
-        this.table=table;
-        this.map=map;
-
+    public BrowserListener(JTable table, JMenu browserMenu, ConcurrentHashMap<Socket, Streams> map) {
+        this.browserMenu = browserMenu;
+        this.table = table;
+        this.map = map;
     }
 
     @Override
@@ -27,7 +26,7 @@ public class BrowserMenuListener implements MenuListener {
         Streams stream = GetSYS.getStream(map,table);
         browserMenu.removeAll();
         assert stream != null;
-        new DiskMenuCreator(browserMenu,stream).createMenu();
+        stream.executor.submit(new DiskMenu(browserMenu, stream));
     }
 
     @Override
